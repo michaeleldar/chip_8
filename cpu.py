d9f1c61030aa62bc300264b3b26698b68b62d834
Michael Halpin <halpinm2010@gmail.com>
2022-06-06 12:41:17 +0100

    .

--- cpu.py
+++ cpu.py
@@ -2,3 +2,105 @@ class CPU:
     def __init__(self, renderer, keyboard):
         self.renderer = renderer
         self.keyboard = keyboard
+
+        self.memory = []  # 4096 byte memory
+        self.v = []  # 16 8-bit registers
+
+        self.i = 0
+
+        self.delayTimer = 0
+
+        self.pc = 0x200
+
+        self.stack = []
+
+        self.paused = False
+
+        self.speed = 10
+
+    def loadSpritesIntoMemory(self):
+        sprites = [
+            0xF0,
+            0x90,
+            0x90,
+            0x90,
+            0xF0,
+            0x20,
+            0x60,
+            0x20,
+            0x20,
+            0x70,
+            0xF0,
+            0x10,
+            0xF0,
+            0x80,
+            0xF0,
+            0xF0,
+            0x10,
+            0xF0,
+            0x10,
+            0xF0,
+            0x90,
+            0x90,
+            0xF0,
+            0x10,
+            0x10,
+            0xF0,
+            0x80,
+            0xF0,
+            0x10,
+            0xF0,
+            0xF0,
+            0x80,
+            0xF0,
+            0x90,
+            0xF0,
+            0xF0,
+            0x10,
+            0x20,
+            0x40,
+            0x40,
+            0xF0,
+            0x90,
+            0xF0,
+            0x90,
+            0xF0,
+            0xF0,
+            0x90,
+            0xF0,
+            0x10,
+            0xF0,
+            0xF0,
+            0x90,
+            0xF0,
+            0x90,
+            0x90,
+            0xE0,
+            0x90,
+            0xE0,
+            0x90,
+            0xE0,
+            0xF0,
+            0x80,
+            0x80,
+            0x80,
+            0xF0,
+            0xE0,
+            0x90,
+            0x90,
+            0x90,
+            0xE0,
+            0xF0,
+            0x80,
+            0xF0,
+            0x80,
+            0xF0,
+            0xF0,
+            0x80,
+            0xF0,
+            0x80,
+            0x80,
+        ]
+
+        for i in range(0, sprites.__len__()):
+            self.memory
